bce7522fc3bf9c55ea5067848dd6121253153b55
Nishant Bihola <nbihola@ualberta.ca>
2025-08-25 23:25:32 -0600

    first-push

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className=' bg-amber-300 h-screen'>
+      <h1 className='text-3xl font-bold underline'>Hello, Next.js!</h1>
+      
+    </div>
+  )
+}
